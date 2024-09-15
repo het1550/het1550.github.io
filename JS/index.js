56b231c48e33ad18c3a9ee5bc0601706f0b4d39a
Het <het.vaghasiya2004@gmail.com>
2024-09-15 21:27:59 +0530

    first commit

---
+++ JS/index.js
@@ -0,0 +1,21 @@
+let index = 0;
+const slides = document.querySelectorAll('.slide');
+setInterval(() => {
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}, 3000);
+
+
+    window.addEventListener('scroll', function() {
+        var navbar = document.querySelector('.glassy-navbar');
+        if (window.scrollY > 50) {
+            navbar.classList.add('navbar-scrolled');
+        } else {
+            navbar.classList.remove('navbar-scrolled');
+        }
+    });
+    
+
+
+ document.getElementsByClassName("btn").style.backgroundColor = "Green";
